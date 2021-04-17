cbfd9b74ece99ddaa355727189b9215176e3f8c0
asifadnan7 <asif.adnan.cse@gmail.com>
2021-04-17 13:00:57 +0600

    added appConfigService and httpRequestInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,10 @@ import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppConfigService } from 'src/services/app.config.service';
+import { AppConfigInitializer } from 'src/services/app.initializer.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequestInterceptor } from './interceptors/http.request.interceptor';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,21 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AkitaNgRouterStoreModule,
     NgbModule
   ],
-  providers: [{ provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' }}],
+  providers: [
+    AppConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: AppConfigInitializer,
+      deps: [AppConfigService],
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true,
+    },
+    { provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/models/app.config.ts
@@ -0,0 +1,4 @@
+export class AppConfig {
+  apiBasePath: string = '';
+  apiKey?: string = '';
+}
