39f76903d064608de979b5c5578df23ef0cba3a3
asifadnan7 <asif.adnan.cse@gmail.com>
2021-04-28 16:00:41 +0600

    fixed eslint error - no unused lifecycle hook mehtods

--- src/app/modules/core/components/page-not-found/page-not-found.component.ts
+++ src/app/modules/core/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
